8b49298aa8e6a7ed3f4695ac4fdbe4a95ca2d2b6
GaryKelly <C00207281@itcarlow.ie>
2018-11-12 10:40:41 +0000

    Removed enum

--- AnimationFSM/Input.cpp
+++ AnimationFSM/Input.cpp
@@ -1,15 +1,1 @@
 #include <Input.h>
-
-Input::Input() 
-{
-	m_current = IDLE;
-}
-Input::~Input() {}
-
-void Input::setCurrent(Action a) {
-	m_current = a;
-}
-
-Input::Action Input::getCurrent() {
-	return m_current;
-}
--- AnimationFSM/Input.h
+++ AnimationFSM/Input.h
@@ -3,21 +3,11 @@
 class Input
 {
 public:
-	Input();
-	~Input();
+	
 
-	enum Action
-	{
-		IDLE,
-		UP,
-		LEFT,
-		RIGHT
-	};
 
-	void setCurrent(Action);
-	Action getCurrent();
 
 private:
-	Action m_current;
+	
 };
 #endif
